45005af2c71c293d424705c63fcd4db3d6cdae0b
Eduardo Bombonatto <eduardoblorenzetti517@gmail.com>
2024-07-17 21:04:59 -0300

    2.2: Course information step 7 (and 2.3 step 8)

--- part2/courseinfo/src/Course.jsx
+++ part2/courseinfo/src/Course.jsx
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import Content from "./Content";
+import Total from "./Total";
 
 const Course = ({ course }) => {
   console.log(course);
@@ -7,9 +8,14 @@ const Course = ({ course }) => {
     <div>
       <Header courseName={course.name} />
       <Content parts={course.parts} />
+      <Total
+        sumOfExercises={course.parts.reduce(
+          (sum, part) => sum + part.exercises,
+          0
+        )}
+      />
     </div>
   );
 };
 
 export default Course;
-    
---
+++ part2/courseinfo/src/Total.jsx
@@ -0,0 +1,5 @@
+const Total = ({ sumOfExercises }) => {
+  return <p>total of {sumOfExercises} exercises</p>;
+};
+
+export default Total;
