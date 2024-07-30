4b144d5ca6b4f4a562d61c6b73ce44b4a0279b4b
Eduardo Bombonatto <eduardoblorenzetti517@gmail.com>
2024-07-30 19:53:48 -0300

    3.5: Phonebook backend step 5

--- part3/index.js
+++ part3/index.js
@@ -55,6 +55,31 @@ app.delete("api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name or number is missing",
+    });
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(newPerson);
+
+  res.json(newPerson);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
