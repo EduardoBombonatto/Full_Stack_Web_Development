e1a9a6865420f82c10852d632853711742df2e2c
Eduardo Bombonatto <eduardoblorenzetti517@gmail.com>
2024-07-31 14:49:28 -0300

    3.9 Phonebook backend step 9

--- part3/index.js
+++ part3/index.js
@@ -1,30 +1,17 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
 
 const persons = [
   {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
+    name: "Eduardo",
+    phone: "6546651681",
     id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
   },
 ];
 
